ef84234165f764d26f8bb45ee471405a610b00e0
msajadi824 <msajadi824@hotmail.com>
2015-02-12 19:49:11 +0330

    fix in checkoutAction for check cookie and save in session and fix it's twig for form and steps.

--- src/Universal/IDTBundle/Controller/CheckoutController.php
+++ src/Universal/IDTBundle/Controller/CheckoutController.php
@@ -7,6 +7,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Cookie;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Universal\IDTBundle\Form\CheckoutType;
 
 class CheckoutController extends Controller
 {
@@ -22,43 +23,24 @@ class CheckoutController extends Controller
 
         $added_items = json_decode(stripcslashes($request->cookies->get("products")), true);
 
-        $valid = true;
-        if(is_null($added_items))
-            $valid = false;
-        else
-            foreach($added_items as &$added_item) {
-                if(
-                    !isset($added_item['product']) ||
-                    !isset($added_item['count']) ||
-                    !isset($added_item['denomination']) ||
-                    !in_array($added_item['type'], array("buy", "recharge"))
-                ){
-                    $valid = false;
-                    break;
-                }
-
-                $added_item['product'] = $added_item['type'] == "buy"
-                    ? $em->getRepository('UniversalIDTBundle:Product')->find($added_item['product'])
-                    : $em->getRepository('UniversalIDTBundle:OrderProduct')->find($added_item['product']);
-
-                if(is_null($added_item['product'])){
-                    $valid = false;
-                    break;
-                }
-
-            }
-
-        $response = new Response();
+        $valid = $this->checkCookie($added_items, $em);
 
         if(!$valid) {
+            $response = new Response("Error in cookies and cleared.");
             $this->get('session')->getFlashBag()->add('error','Error occurred in Cookies.');
-            $added_items = array();
             $response->headers->setCookie(new Cookie("products", "[]",0,"/",null,false,false ));
+
+            return $response;
         }
 
+        $this->get('session')->set('basket', json_decode(stripcslashes($request->cookies->get("products")), true));
+
+        $form = $this->createForm(new CheckoutType());
+
         return $this->render('UniversalIDTBundle:Checkout:checkout.html.twig', array(
-                'data' => $added_items
-            ), $response);
+                'data' => $added_items,
+                'form' => $form->createView()
+            ));
     }
 
     public function testAction(Request $request)
@@ -75,5 +57,38 @@ class CheckoutController extends Controller
         $response = new Response("aaaa");
         $response->headers->setCookie(new Cookie("products", json_encode($data),0,"/",null,false,false ));
         return $response;
+    }
+
+    /**
+     * @param array $added_items
+     * @param EntityManager $em
+     * @return bool
+     */
+    private function checkCookie(array &$added_items, EntityManager $em)
+    {
+        if(is_null($added_items))
+            return false;
+//        die("is null");
+
+        foreach($added_items as &$added_item) {
+            if(
+                !isset($added_item['product']) ||
+                !isset($added_item['count']) ||
+                !isset($added_item['denomination']) ||
+                !in_array($added_item['type'], array("buy", "recharge"))
+            )
+                return false;
+//                die("field error");
+
+            $added_item['product'] = $added_item['type'] == "buy"
+                ? $em->getRepository('UniversalIDTBundle:Product')->find($added_item['product'])
+                : $em->getRepository('UniversalIDTBundle:OrderProduct')->find($added_item['product']);
+
+            if(is_null($added_item['product']))
+                return false;
+//                die("not found id");
+        }
+
+        return true;
     }
 }
---
+++ src/Universal/IDTBundle/Form/CheckoutType.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Universal\IDTBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+use Universal\IDTBundle\DBAL\Types\PaymentMethodEnumType;
+
+class CheckoutType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('email', 'email', array(
+                    'required' => false
+                ))
+            ->add('sms', 'text', array(
+                    'required' => false
+                ))
+            ->add('method', 'choice', array(
+                    'choices' => array(PaymentMethodEnumType::OGONE => "ogone", PaymentMethodEnumType::SOFORT => "sofort"),
+                    'expanded' => true,
+                    'required' => true
+                ))
+            ->add('submit', 'submit', array(
+                    'label' => 'Place an order'
+                ))
+        ;
+    }
+    
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+//            'data_class' => 'Universal\IDTBundle\Entity\Product'
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'universal_idt_rates';
+    }
+}
