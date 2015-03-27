b0c05537a9253ba08baeae947bc6e5db4e41e6af
msajadi824 <msajadi824@hotmail.com>
2015-03-27 17:54:33 +0430

    fixes in CSV and Image import

--- src/Universal/IDTBundle/Command/ImportProductsCommand.php
+++ src/Universal/IDTBundle/Command/ImportProductsCommand.php
@@ -6,6 +6,7 @@ use Doctrine\ORM\EntityManager;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
 use Universal\IDTBundle\Entity\Product;
 
 class ImportProductsCommand extends AbstractImportCommand
@@ -45,6 +46,10 @@ class ImportProductsCommand extends AbstractImportCommand
             $product->setClassId($row['cid']);
             $product->setDenominations(array($row['denom1'],$row['denom2'],$row['denom3']));
             $product->setFreeAmountDenomination1($row['free_amount_denom1']);
+//            $product->addAccessNumber()
+//            $product->setFile(new UploadedFile("C:\\xampp\\htdocs\\UniversalIdt\\web\\uploads\\test\\(".$i.").png", "(".$i.").png", 'image/png', 70, null, true));
+//            $product->setDialingInstructions();
+//            $product->setGeneralInformation();
             $em->persist($product);
             $i++;
         }
