f4abe23d4c0ddf5c0f15031e8ffd44c1dcbcdef9
msajadi824 <msajadi824@hotmail.com>
2015-04-14 21:19:56 +0430

    new design for app_layout, breadcrumbs, menus and ogoneTemplate.

--- src/Universal/IDTBundle/Menu/Builder.php
+++ src/Universal/IDTBundle/Menu/Builder.php
@@ -7,52 +7,85 @@ use Symfony\Component\DependencyInjection\ContainerAware;
 
 class Builder extends ContainerAware
 {
-    public function mainMenu(FactoryInterface $factory)
+    public function userMenu(FactoryInterface $factory)
     {
-        $granted = $this->container->get('security.authorization_checker')->isGranted('IS_AUTHENTICATED_REMEMBERED');
+        $menu = $factory->createItem('root', array('childrenAttributes' => array('class' => 'nav nav-pills nav-stacked')));
 
-        $menu = $factory->createItem('root', array('childrenAttributes' => array('class' => 'mainnav')));
+        $menu->addChild('Home', array(
+                'route' => 'user_home',
+                'extras' => array('icon' => 'home'),
+                'label' => $this->container->get('translator')->trans('menu.navbar.home',[],'application')
+            ));
 
-        if($granted)
-            $menu->addChild('Home', array(
-                    'route' => 'user_home',
-                    'extras' => array('icon' => 'home'),
-                    'label' => $this->container->get('translator')->trans('menu.navbar.home',[],'application')
-                ));
-        else
-            $menu->addChild('Home', array(
-                    'route' => 'WebPage_main',
-                    'extras' => array('icon' => 'home'),
-                    'label' => $this->container->get('translator')->trans('menu.navbar.home',[],'application')
-                ));
+        $menu->addChild('My PINs', array(
+                'route' => 'user_pins',
+                'extras' => array('icon' => 'th'),
+                'label' => $this->container->get('translator')->trans('menu.navbar.my_pins',[],'application')
+            ));
 
-        if($granted) {
-            $menu->addChild('My PINs', array(
-                    'route' => 'user_pins',
-                    'extras' => array('icon' => 'th'),
-                    'label' => $this->container->get('translator')->trans('menu.navbar.my_pins',[],'application')
-                ));
-            $menu->addChild('Billing History', array(
-                    'route' => 'user_orders',
-                    'extras' => array('icon' => 'copy'),
-                    'label' => $this->container->get('translator')->trans('menu.navbar.billing_history',[],'application')
-                ));
-        }
+        $menu->addChild('Billing History', array(
+                'route' => 'user_orders',
+                'extras' => array('icon' => 'copy'),
+                'label' => $this->container->get('translator')->trans('menu.navbar.billing_history',[],'application')
+            ));
 
         $menu->addChild('Calling Cards', array(
                 'route' => 'WebPage_main',
                 'extras' => array('icon' => 'credit-card', 'route_label' => 'calling-cards'),
                 'label' => $this->container->get('translator')->trans('menu.navbar.calling_cards',[],'application')
             ));
+
         $menu->addChild('Rates', array(
                 'route' => 'WebPage_main',
                 'extras' => array('icon' => 'external-link', 'route_label' => 'rates'),
                 'label' => $this->container->get('translator')->trans('menu.navbar.rates',[],'application')
             ));
+
         $menu->addChild('Help', array(
                 'route' => 'WebPage_main',
-                'extras' => array('icon' => 'info-sign', 'route_label' => 'contact'),
+                'extras' => array('icon' => 'info-circle', 'route_label' => 'contact'),
                 'label' => $this->container->get('translator')->trans('menu.navbar.help',[],'application')
+            ));
+
+        return $menu;
+    }
+
+    public function mainMenu(FactoryInterface $factory)
+    {
+        $menu = $factory->createItem('root', array('childrenAttributes' => array('class' => 'nav navbar-nav navbar-right')));
+
+        $menu->addChild('Home', array(
+                'route' => 'WebPage_main',
+            ));
+
+        $menu->addChild('Calling Cards', array(
+                'route' => 'WebPage_main',
+                'extras' => array('route_label' => 'calling_cards'),
+//                'label' => $this->container->get('translator')->trans('menu.navbar.calling_cards',[],'webpage')
+            ));
+
+        $menu->addChild('Rates', array(
+                'route' => 'WebPage_main',
+                'extras' => array('route_label' => 'rates'),
+//                'label' => $this->container->get('translator')->trans('menu.navbar.rates',[],'webpage')
+            ));
+
+        $menu->addChild('Applications', array(
+                'route' => 'WebPage_main',
+                'extras' => array('route_label' => 'application'),
+//                'label' => $this->container->get('translator')->trans('menu.navbar.application',[],'webpage')
+            ));
+
+        $menu->addChild('Basket', array(
+                'route' => 'WebPage_main',
+                'extras' => array('route_label' => 'basket'),
+//                'label' => $this->container->get('translator')->trans('menu.navbar.basket',[],'webpage')
+            ));
+
+        $menu->addChild('Contact', array(
+                'route' => 'WebPage_main',
+                'extras' => array('route_label' => 'contact'),
+//                'label' => $this->container->get('translator')->trans('menu.navbar.contact',[],'webpage')
             ));
 
         return $menu;
